f7f79c7207e267a9acc90a1bf83fcbdd9267ed63
sosinatolossa <sosinatolossa16@gmail.com>
2021-05-01 11:16:42 -0500

    just put back [Authorize] in user controller

--- YeMigbeKeeper/Controllers/UserController.cs
+++ YeMigbeKeeper/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using YeMigbeKeeper.Repositories;
 
 namespace YeMigbeKeeper.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
