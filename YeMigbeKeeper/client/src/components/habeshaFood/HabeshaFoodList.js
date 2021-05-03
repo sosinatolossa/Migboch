e2c1fb277ff61fd38f52968a9720fe7c626a8dfc
sosinatolossa <sosinatolossa16@gmail.com>
2021-05-03 16:38:18 -0500

    added some reactstrap

--- YeMigbeKeeper/client/src/components/habeshaFood/HabeshaFoodList.js
+++ YeMigbeKeeper/client/src/components/habeshaFood/HabeshaFoodList.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { HabeshaFoodContext } from "./HabeshaFoodProvider";
 import HabeshaFood from "./HabeshaFoodCard";
+import { Row, Col, Container } from "reactstrap";
 
 const HabeshaFoodList = () => {
     const { habeshaFoods, getAllHabeshaFoods } = useContext(HabeshaFoodContext);
@@ -12,11 +13,13 @@ const HabeshaFoodList = () => {
     // Maps through each habesh food object, sending 
     // them to be converted to HTML then prints them all out.
     return (
-        <section>
-            {habeshaFoods.map((habeshaFoodObj) => (
-                <HabeshaFood key={habeshaFoodObj.id} habeshaFood={habeshaFoodObj} /> //habeshaFood is our parameter(object) in HabeshaFoodCard
-            ))}
-        </section>
+        <Container>
+            <Row>
+                {habeshaFoods.map((habeshaFoodObj) => (
+                    <Col md="4"><HabeshaFood key={habeshaFoodObj.id} habeshaFood={habeshaFoodObj} /></Col> //habeshaFood is our parameter(object) in HabeshaFoodCard
+                ))}
+            </Row>
+        </Container>
     );
 };
 
