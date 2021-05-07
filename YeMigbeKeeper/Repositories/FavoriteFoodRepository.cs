55c9f801f5515c3c1312715dc104575ef70462d0
sosinatolossa <sosinatolossa16@gmail.com>
2021-05-07 13:45:45 -0500

    just put back lost files

--- YeMigbeKeeper/Repositories/FavoriteFoodRepository.cs
+++ YeMigbeKeeper/Repositories/FavoriteFoodRepository.cs
@@ -19,7 +19,7 @@ namespace YeMigbeKeeper.Repositories
                 {
                     cmd.CommandText = @"
                         SELECT ff.Id, ff.UserId, ff.HabeshaFoodId,
-	                hf.TypeId, hf.Picture, hf.[Name], hf.[Description], hf.Ingredient,
+	                hf.TypeId, hf.Picture, hf.Name as habeshaFoodName, hf.[Description], hf.Ingredient,
                                 hf.TotalCalorie, hf.TotalFat, hf.Cholesterol, hf.Sodium, hf.TotalCarbohydrate,
                                 hf.Protein, hf.Calcium, hf.Iron, hf.Potassium, hf.UserId,
 	                t.[Name] as TypeName,
@@ -53,7 +53,7 @@ namespace YeMigbeKeeper.Repositories
                                     Name = DbUtils.GetString(reader, "TypeName"),
                                 },
                                 Picture = DbUtils.GetString(reader, "Picture"),
-                                Name = DbUtils.GetString(reader, "Name"),
+                                Name = DbUtils.GetString(reader, "habeshaFoodName"),
                                 Description = DbUtils.GetString(reader, "Description"),
                                 Ingredient = DbUtils.GetString(reader, "Ingredient"),
                                 TotalCalorie = DbUtils.GetNullableInt(reader, "TotalCalorie"),
